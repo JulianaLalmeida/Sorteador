5c28f2969c5b11253dc8e1c0f9f016c9cdc5316e
JulianaLalmeida <julala_bel@hotmail.com>
2025-02-28 22:09:58 -0300

    Nova função

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".inputMin").value)
     const max = Math.floor(document.querySelector(".inputMax").value)
 
-    const result = Math.floor(Math.random()*(max-min+1))+min;
+        if(min < max){
+            const result = Math.floor(Math.random()*(max-min+1))+min;
+            document.getElementById("result").textContent= result
+        }
+        else{
+            alert("O valor mínimo tem que ser MENOR que o valor máximo!")
+        }
 
-    document.getElementById("result").textContent= result
 }
